1f203014a13b352eafbbdb389064cabeb6bbc411
Matthias Koenig <konigmatt@googlemail.com>
2016-03-19 13:55:04 +0100

    small changeds

--- lib/local_maven_repo.sh
+++ lib/local_maven_repo.sh
@@ -2,5 +2,5 @@
 # change in the lib directory
 cd $CY3SBML/target
 # add to local repository
-mvn install:install-file -DgroupId=cy3sbml-temp -DartifactId=cy3sbml -Dversion=0.1.6 -Dfile=cy3sbml-0.1.6.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=cy3sbml-temp -DartifactId=cy3sbml -Dversion=0.1.7 -Dfile=cy3sbml-0.1.7.jar -Dpackaging=jar -DgeneratePom=true
 
